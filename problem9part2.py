3eaf068656705dbcb02f030a0b99a8871976d092
Bhavik Sachdev <b.sachdev1904@gmail.com>
2023-07-25 18:41:27 +0530

    problem 9 part 2

--- problem9part2.py
+++ problem9part2.py
@@ -1,22 +1,23 @@
 import copy
 
 def main():
-    knots = [Knot() for i in range(10)]
-    explored = []
-    with open("sample92.txt") as file:
+    with open("input9.txt") as file:
         commands = file.readlines()
+    knots = [Knot() for _ in range(10)]
+    explored = []
     for command in commands:
         if '\n' in command:
             command = command.replace('\n','')
         direction, steps = command.split(" ")
         steps: int = int(steps)
-        for j in range(1, steps + 1):
-            print(j)
-            for i in range(9, 0, -1):
-                knots[i], knots[i - 1] = move(knots[i], knots[i - 1], direction)
-                for knot in knots:
-                    print(f"({knot})", end=" ")
-                print()
+        for _ in range(steps):
+            knots[9] = move_head(knots[9], direction)
+            for i in range(8, -1, -1):
+                knots[i] = move_tail(knots[i + 1], knots[i])
+            tail = knots[0].position()
+            if tail not in explored:
+                explored.append(tail)
+    print(len(explored))        
 
 class Knot:
     def __init__(self, x = 1, y = 1) -> None:
@@ -24,7 +25,7 @@ class Knot:
         self.y = y
 
     def __str__(self) -> str:
-        return f"{self.x}, {self.y}"
+        return f"({self.x}, {self.y})"
     
     def position(self):
         return (self.x, self.y)
@@ -32,55 +33,44 @@ class Knot:
 def difference(head, tail):
     return ((head.x - tail.x), (head.y - tail.y))
 
-def move(head, tail, direction):
-    new_head = copy.deepcopy(head)
-    new_tail = copy.deepcopy(tail)
-    diff = difference(head, tail)
+def move_head(head, direction):
+    new = copy.deepcopy(head)
     if direction == "R":
-        if diff == (1, 0):
-            new_tail.x += 1
-        elif diff == (1, 1):
-            new_tail.x += 1
-            new_tail.y += 1
-        elif diff == (1, -1):
-            new_tail.x += 1
-            new_tail.y -= 1
-        new_head.x += 1
-    
+        new.x += 1
     elif direction == "L":
-        if diff == (-1, 0):
-            new_tail.x -= 1
-        elif diff == (-1, 1):
-            new_tail.x -= 1
-            new_tail.y += 1
-        elif diff == (-1, -1):
-            new_tail.x -= 1
-            new_tail.y -= 1
-        new_head.x -= 1
-    
+        new.x -= 1
     elif direction == "U":
-        if diff == (0, 1):
-            new_tail.y += 1
-        elif diff == (1, 1):
-            new_tail.y += 1
-            new_tail.x += 1
-        elif diff == (-1, 1):
-            new_tail.x -= 1
-            new_tail.y += 1
-        new_head.y += 1
-
+        new.y += 1
     elif direction == "D":
-        if diff == (0, -1):
-            new_tail.y -= 1
-        elif diff == (-1, -1): 
-            new_tail.x -= 1
-            new_tail.y -= 1
-        elif diff == (1, -1):
-            new_tail.x += 1
-            new_tail.y -= 1
-        new_head.y -= 1
-    return new_head, new_tail
-    
+        new.y -= 1
+    return new
+
+def move_tail(new, tail):
+    new_tail = copy.deepcopy(tail)
+    diff = difference(new, new_tail)
+
+    if diff == (2,2) or diff == (1,2) or diff == (2, 1):
+        new_tail.x += 1
+        new_tail.y += 1
+    elif diff == (0, 2):
+        new_tail.y += 1
+    elif diff == (0, -2):
+        new_tail.y -= 1
+    elif diff == (2, 0):
+        new_tail.x += 1
+    elif diff == (-2, 0):
+        new_tail.x -= 1
+    elif diff == (2, -1) or diff == (2, -2) or diff == (1, -2):
+        new_tail.x += 1
+        new_tail.y -=1 
+    elif diff == (-2, -1) or diff == (-2, -2) or diff == (-1, -2):
+        new_tail.x -= 1
+        new_tail.y -= 1
+    elif diff == (-2, 1) or diff == (-2, 2) or diff == (-1, 2):
+        new_tail.x -= 1
+        new_tail.y += 1
+
+    return new_tail
+
 if __name__ == "__main__":
     main()
-
