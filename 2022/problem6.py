314acaee94aa3faedb3f4064e0d53dd04c058ca5
Bhavik Sachdev <b.sachdev1904@gmail.com>
2023-12-07 12:21:35 +0530

    reorganising

--- 2022/problem6.py
+++
@@ -1,9 +0,0 @@
-with open("inputday6", "r") as file:
-    for line in file:
-        length = len(line)
-        for i in range(0, length):
-            characters = line[i : i + 14]
-            characters_set = set(characters)
-            if len(characters_set) == 14:
-                print(i + 14)
-                break
