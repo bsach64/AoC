1bf2ae8e87b1908d20ebf2a60d9b191abf208bff
Bhavik Sachdev <b.sachdev1904@gmail.com>
2025-12-01 17:19:18 +0530

    day 1: bruteforcing my way through life

---
+++ 2025/src/main.rs
@@ -0,0 +1,48 @@
+use std::fs;
+
+fn day1(filename: &str) -> u64 {
+    let contents = fs::read_to_string(filename)
+        .expect("could not open input file");
+
+    let lines = contents.lines();
+    let mut res = 0;
+    let mut cur = 50;
+
+    for line in lines {
+        let mut chars = line.chars();
+
+        match chars.nth(0).expect("did not get direction") {
+            'L' => {
+                let diff: i64 = chars.as_str().parse().expect("could not get integer");
+                for _ in 0..diff {
+                    assert!(cur >= 0 && cur <= 99, "value of cur: {}", cur);
+                    if cur == 0 {
+                        cur = 99;
+                    } else {
+                        cur -= 1;
+                        if cur == 0 {
+                            res += 1;
+                        }
+                    }
+                }
+            },
+            'R' => {
+                let diff: i64 = chars.as_str().parse().expect("could not get integer");
+                for _ in 0..diff {
+                    assert!(cur >= 0 && cur <= 99);
+                    cur += 1;
+                    if cur == 100 {
+                        res += 1;
+                        cur = 0;
+                    }
+                }
+            },
+            _ => println!("Invalid Direction")
+        }
+    }
+    res
+}
+
+fn main() {
+    println!("{}", day1("src/1.txt"));
+}
